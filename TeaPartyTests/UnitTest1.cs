7e41e4fa732f8af5f49957c29fb39ee7a282e385
Magnus <magnusb@tmsoftware.is>
2014-12-11 21:36:44 +0000

    Initial

---
+++ TeaPartyTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeaPartyTests
+{
+   [TestClass]
+   public class UnitTest1
+   {
+      TeaPartyKata target;
+
+      [TestInitialize]
+      public void Setup()
+      {
+         target = new TeaPartyKata();
+      }
+
+      [TestMethod]
+      public void isWoman()
+      {
+         var name = "Gugga";
+         Assert.AreEqual(target.Welcome(name, true, false), "Hello Ms. " + name);
+      }
+
+      [TestMethod]
+      public void isMan()
+      {
+         Assert.AreEqual(target.Welcome("Thatcher", false, false), "Hello Mr. Thatcher");
+      }
+
+      [TestMethod]
+      public void KnightlyisWoman()
+      {
+         Assert.AreEqual(target.Welcome("Knightly", true, false), "Hello Ms. Knightly");
+      }
+
+      [TestMethod]
+      public void MirrenisDame()
+      {
+         Assert.AreEqual(target.Welcome("Mirren", true, true), "Hello Dame Mirren");
+      }
+
+      [TestMethod]
+      public void ConneryisSir()
+      {
+         var name = "Connery";
+         Assert.AreEqual(target.Welcome(name, false, true), "Hello Sir " + name);
+      }
+
+   }
+}
---
+++ TheTeaPartyKata/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Net.Configuration;
+using System.Runtime.Remoting.Messaging;
+
+public class TeaPartyKata : ITeaPartyKata
+{
+   public string Welcome(string lastName, bool isWoman, bool isKnighted)
+   {
+      return lastName.IsKnight(isKnighted).Greeting(isWoman, lastName);
+   }
+}
+
+public static class extStr
+{
+   private static string NobleTitle(bool isWoman)
+   {
+      return (isWoman ? "Dame " : "Sir ");
+   }
+
+   private static string StandardTitle(bool isWoman)
+   {
+      return (isWoman ? "Ms. " : "Mr. ");
+   }
+
+   public static bool IsKnight(this string s, bool isKnighted)
+   {
+      return isKnighted;
+   }
+
+   public static string Greeting(this bool isKnight, bool isWoman, string sName)
+   {
+     return "Hello " + (isKnight ? NobleTitle(isWoman) : StandardTitle(isWoman)) + sName;
+   }
+}
+
+public interface ITeaPartyKata
+{
+   /// <summary>
+   /// Welcome a guest
+   /// </summary>
+   /// <param name="lastName">the last name of the guest</param>
+   /// <param name="isWoman"><c>true</c> if the guest is female</param>
+   /// <param name="isSir"><c>true</c> if the guest was knighted by the queen</param>
+   /// <returns>issues welcome text to the guest</returns>
+   string Welcome(string lastName, bool isWoman, bool isSir);
+}
